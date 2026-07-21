7e8891460258003a72e2979b4af4d95281e5834c
wxh <wxhmessage@live.cn>
2026-07-21 10:06:20 +0800

    软件版本升级到1.0.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.lonepheasantwarrior.talkify"
         minSdk = 30
         targetSdk = 37
-        versionCode = 28
-        versionName = "1.0.26"
+        versionCode = 29
+        versionName = "1.0.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
